5957bb577ee6af3d444d16068466bab201777f61
emav-developer <71562233+emav-developer@users.noreply.github.com>
2021-01-24 20:21:36 +0100

    hasta disparo.
    Disparo y control con Easy Touch Control

---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float velocitat = 10f;   	       // velocitat de la nau
+    Vector2 movi = new Vector2();   // per calcular moviment
+    Rigidbody2D rb;                 // per accedir al component RigidBody
+    [SerializeField] float cadencia = 0.5f; // dispararà cada 5 dècimes de segon
+    float crono = 0f;	  // per comptar el temps de cadència
+
+    [SerializeField]
+    GameObject missil; // element a instanciar. Arrosseguem prefab!
+    [SerializeField]
+    Transform cano;    // d'on surt el tretSerializeField]
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Damos valor a rb
+    }
+
+    void Update()
+    {
+        movi.x = ETCInput.GetAxis("Horizontal") * velocitat;
+        movi.y = ETCInput.GetAxis("Vertical") * velocitat;
+
+        if (ETCInput.GetButton("Shoot") && crono > cadencia) // Shoot: nom del control
+            Dispara();
+        crono += Time.deltaTime;
+        // la següent línea permet disparar ràpid amb múltiples clicks
+        if (ETCInput.GetButtonUp("Shoot")) crono = cadencia;
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movi;    // Apliquem velocitat. No utilitzar Translate (fisiques!)
+    }
+    void Dispara()
+    {
+        crono = 0;
+        Instantiate(missil, cano.position, cano.rotation);
+    }
+
+}
---
+++ Assets/Scripts/ScrShot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrShot : MonoBehaviour
+{
+    [SerializeField]
+    float vel = 20f;
+
+    void Start()
+    {
+        Destroy(gameObject, 3); // per si no col·lisiona amb res
+    }
+    void Update()
+    {
+        transform.Translate(vel * Time.deltaTime, 0, 0);
+    }
+
+}
