e1ee20b88ea05c22c32baa7bfd8f90482912c5b0
emav-developer <71562233+emav-developer@users.noreply.github.com>
2021-04-04 19:38:57 +0200

    UI
    Todo menos botones (se hizo en EMAV)

---
+++ Assets/6. Imatges Filled/BarraVida.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BarraVida : MonoBehaviour {
+
+
+	[SerializeField] Image blanca, sakura;
+    enum Personajes {Blanca,Sakura};
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadPlus)) CanviaVida(Personajes.Blanca, 0.1f);
+        if (Input.GetKeyDown(KeyCode.KeypadMinus)) CanviaVida(Personajes.Blanca, -0.1f);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) CanviaVida(Personajes.Sakura, -0.1f);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) CanviaVida(Personajes.Sakura, 0.1f);
+    }
+
+    void CanviaVida(Personajes qui, float quant)
+	{
+        // No s'han de verificar que els valors estiguin entre 0 i 1, se n'encarrega Unity
+        if (qui == Personajes.Blanca) blanca.fillAmount += quant;
+            else sakura.fillAmount += quant;
+	}
+
+
+}
---
+++ Assets/6. Imatges Filled/ScrBarraVida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrBarraVida : MonoBehaviour
+{
+    [SerializeField] Image blanka, sakura;
+    enum Personatges { Blanka, Sakura };
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadPlus)) CanviaVida(Personatges.Blanka, 0.1f);
+        if (Input.GetKeyDown(KeyCode.KeypadMinus)) CanviaVida(Personatges.Blanka, -0.1f);
+        if (Input.GetKeyDown(KeyCode.Alpha1)) CanviaVida(Personatges.Sakura, -0.1f);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) CanviaVida(Personatges.Sakura, 0.1f);
+    }
+
+    void CanviaVida(Personatges qui, float quant)
+    {
+        // No s'han de verificar que els valors estiguin entre 0 i 1, se n'encarrega Unity
+        if (qui == Personatges.Blanka) blanka.fillAmount += quant;
+        else sakura.fillAmount += quant;
+    }
+
+}
